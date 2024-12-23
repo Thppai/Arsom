04f29bc34a04ed15aac642c0a8c668d407d32b8a
Thppai <thanathip-b@rmutp.ac.th>
2024-12-23 17:56:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="shortcut icon" href="img/logo.jpg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+
+<style>
+    footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        
+    }
+    img {
+        height: 30%;
+
+    }
+</style>   
+ 
+
+</head>
+<body>
+<br><br><br><br>
+<div class="px-4 py-4 my-5 text-center">
+<img class="d-block mx-auto mb-4" src="img/banner.jpeg" width="1500" height="">
+<div class="col-lg-6 mx-auto">
+  <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <button type="button" onclick="location.href='login.php'" class="btn btn-primary btn-lg px-4 gap-3">Login</button>
+    <button type="button" onclick="location.href='view_product.php'"  class="btn btn-outline-secondary btn-lg px-4">View Product</button>
+  </div>
+</div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
